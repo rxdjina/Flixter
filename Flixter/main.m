1836eaee4e943f21656f2ff611f9b7ad5a409f70
rxdjina <55474317+rxdjina@users.noreply.github.com>
2022-06-22 13:11:36 -0700

    Uploaded final Flixter Project

---
+++ Flixter/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  Flixter
+//
+//  Created by Rodjina Pierre Louis on 6/16/22.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
+
+I've uploaded the unzipped version! Sorry for taking so long, I accidentally deleted my entire project while using github desktop.
